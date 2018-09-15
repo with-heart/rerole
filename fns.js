97b32e2e9bf0c0a524b2d73e22aedb4427cf80c1
with.heart <with.heart@protonmail.com>
2018-09-15 02:29:36 -0400

    feat: Create functions for reading config

---
+++ fns.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+const os = require('os')
+const path = require('path')
+
+const OPTIONS_REGEX = /\[(default)|profile (.*)\]/g
+
+const readConfigFile = () => {
+  const file = path.join(os.homedir(), '.aws', 'config')
+  return fs.readFileSync(file, 'utf8')
+}
+
+const findRoleNames = contents => {
+  const matches = []
+  let match = OPTIONS_REGEX.exec(contents)
+
+  while (match !== null) {
+    if (match[1] || match[2]) {
+      matches.push(match[1] || match[2])
+    }
+    match = OPTIONS_REGEX.exec(contents)
+  }
+
+  return matches
+}
+
+module.exports = {
+  readConfigFile,
+  findRoleNames,
+}
---
+++ test.js
@@ -0,0 +1,45 @@
+const fs = require('fs')
+const os = require('os')
+const {readConfigFile, findRoleNames} = require('./fns')
+
+jest.mock('fs')
+jest.mock('os')
+
+const fileContents = `
+[default]
+key=value
+
+[profile profile1]
+key=value
+
+[profile profile2]
+key=value
+
+[profile pro.file-3]
+key=value
+`
+
+afterEach(() => {
+  jest.resetAllMocks()
+})
+
+test('readConfigFile returns contents of ~/.aws/config', () => {
+  const homeDirectory = '/Users/username'
+
+  os.homedir.mockReturnValue(homeDirectory)
+  fs.readFileSync.mockReturnValue(fileContents)
+
+  const contents = readConfigFile()
+
+  expect(contents).toEqual(fileContents)
+  expect(fs.readFileSync).toHaveBeenCalledWith(
+    `${homeDirectory}/.aws/config`,
+    'utf8',
+  )
+})
+
+test('findRoleNames returns all valid role names from config file', () => {
+  const roleNames = findRoleNames(fileContents)
+
+  expect(roleNames).toEqual(['default', 'profile1', 'profile2', 'pro.file-3'])
+})
